b5a67baffd1d9dd1e636c1dc5215e4cbfc15050d
任正阳 <715134026@qq.com>
2019-03-19 22:08:52 +0800

    Initial Commit

---
+++ PageBroadcast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageBroadcast
+//
+//  Created by ren on 2019/3/19.
+//  Copyright © 2019年 ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageBroadcast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PageBroadcast
+//
+//  Created by ren on 2019/3/19.
+//  Copyright © 2019年 ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
